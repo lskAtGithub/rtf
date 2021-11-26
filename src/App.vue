ca3a84f18925a0131c6048b9d7c9431e8526a9c8
liusk <949919743@qq.com>
2021-11-26 11:36:39 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="components-container">
+    <div>
+      <tinymce v-model="content" :height="300" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Tinymce from '@/components/Tinymce'
+
+export default {
+  name: 'TinymceDemo',
+  components: { Tinymce },
+  data() {
+    return {
+      content:""
+    }
+  }
+}
+</script>
+
+<style scoped>
+.editor-content{
+  margin-top: 20px;
+}
+</style>
+
---
+++ src/components/Tinymce/toolbar.js
@@ -0,0 +1,6 @@
+// 插件配置地址： https://www.tinymce.com/docs/advanced/editor-control-identifiers/#toolbarcontrols
+
+// emoticons 表情
+const toolbar = ['searchreplace bold italic underline strikethrough alignleft aligncenter alignright outdent indent  blockquote undo redo removeformat subscript superscript code codesample', 'hr bullist numlist link image charmap preview anchor pagebreak insertdatetime media table forecolor backcolor fullscreen']
+
+export default toolbar
